4abb4cd524b3f012edfae070b0145856c83332cf
Peter Graham <peterghm@gmail.com>
2012-02-10 22:14:15 -0500

    show flashes

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,7 +1,11 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Orzbot.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def format_date(datet)
+    if datet.nil?
+      "[none]"
+    else
+      datet.strftime("%Y/%m/%d %H:%M")
+    end
+  end
 end

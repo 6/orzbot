8d2017abc5ed645964ef58bc60eedfaa91ce599c
Peter Graham <peterghm@gmail.com>
2012-02-12 23:44:08 -0500

    wday diff and JST time creator helpers

--- lib/time_helper.rb
+++ lib/time_helper.rb
@@ -6,4 +6,19 @@ module TimeHelper
   def self.to_jst(time)
     time.in_time_zone('Asia/Tokyo')
   end
+  
+  def self.create_time_jst(year, month, day, hour, minute)
+    # TODO ruby way to do this?
+    Time.zone = "Asia/Tokyo"
+    Chronic.time_class = Time.zone
+    Chronic.parse("#{year}/#{month}/#{day} #{hour}:#{minute} +09:00")
+  end
+  
+  def self.wday_diff(day1, day2, forwards)
+    if forwards
+      (0..6).to_a.rotate(day1).index(day2)
+    else
+      (0..6).to_a.rotate(day2).index(day1)
+    end
+  end
 end

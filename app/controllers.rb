0478aebda9b4c3b75c41a842ee75a13753c9a199
Peter Graham <peterghm@gmail.com>
2012-02-13 01:37:49 -0500

    woops - don't lowercase japanese

--- app/controllers.rb
+++ app/controllers.rb
@@ -56,7 +56,7 @@ Orzbot.controllers  do
   
   get :home, :map => "/(:locale)", :provides => [:html, :rss] do
     I18n.locale = get_locale(params[:locale])
-    @animes = Anime.all(:order => get_locale(params[:locale]) == :ja ? 'lower(title_ja) ASC' : 'lower(title_en) ASC')
+    @animes = Anime.all(:order => get_locale(params[:locale]) == :ja ? 'title_ja ASC' : 'lower(title_en) ASC')
     render :home
   end
 end

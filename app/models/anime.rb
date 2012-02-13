ca1d97093850956d053fb8fbd6d68f35ed2b1a2d
Peter Graham <peterghm@gmail.com>
2012-02-12 22:10:14 -0500

    useful methods for anime model

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -6,4 +6,18 @@ class Anime < ActiveRecord::Base
     start = read_attribute(:start_date)
     start ? TimeHelper.to_jst(start) : nil
   end
+  
+  def started_airing?
+    self.start_date - Time.now > 0 ? false : true
+  end
+  
+  def numeric_air_days
+    self.air_days.split(",").map{|wday_s|
+      {"Mo"=>1,"Tu"=>2,"We"=>3,"Th"=>4,"Fr"=>5,"Sa"=>6,"Su"=>0}[wday_s]
+    }.sort
+  end
+  
+  def parsed_ignore_dates
+    self.ignore_dates.split(",").map{|i| Chronic.parse(i)}.sort
+  end
 end
